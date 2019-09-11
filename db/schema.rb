8ec9ee6357665f4c367ab310dfcb96ad2563f44d
S-Toky <stokyr29@gmail.com>
2019-09-11 15:21:20 +0300

    ajout du front page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_11_060116) do
 
   # These are extensions that must be enabled in order to support this database
@@ -65,18 +64,12 @@ ActiveRecord::Schema.define(version: 2019_09_11_060116) do
     t.datetime "updated_at", null: false
     t.index ["mark_id"], name: "index_models_on_mark_id"
   end
-​
-76
-  # These are extensions that must be enabled in order to support this database
-77
-  enable_extension "plpgsql"
+
   create_table "types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -91,7 +84,5 @@ ActiveRecord::Schema.define(version: 2019_09_11_060116) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-
 
 end
