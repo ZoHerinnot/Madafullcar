9c486b1044f70db3babfef326b2687ce03f51604
tokynomenaja <nomenjatoky21@gmail.com>
2019-09-19 16:44:33 +0300

    Correct bug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-	def facebook
-        puts "*"*60
-        	puts request.env["omniauth.auth"].inspect
-        puts "*"*60
+  def facebook
         @user = UserProvider.find_for_facebook_oauth(request.env["omniauth.auth"])
         if @user.persisted?
           sign_in_and_redirect @user, :event => :authentication               
@@ -13,7 +10,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
     def google_oauth2
         @user = UserProvider.find_for_google_oauth(request.env["omniauth.auth"])
-        puts request.env["omniauth.auth"].inspect
+
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication
         else
@@ -22,7 +19,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         end
     end
     def failure
-       	redirect_to "/users/sign_in"
+        redirect_to "/users/sign_in"
     end
 
 end
